ab5cc3d80b88716e24782aafd71abdbe52ce0973
Tobias J <tobias.juhre@bio.uni-giessen.de>
2017-08-14 19:36:40 +0200

    script optimizing

--- ePros_parser/check_contacts_otf.py
+++ ePros_parser/check_contacts_otf.py
@@ -4,12 +4,6 @@
 import argparse
 import sys
 import os
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
-import seaborn as sns
-import collections
 import timeit
 import re
 from pprint import pprint
@@ -36,36 +30,11 @@ def insert_into_data_structure(key, value, dict):
     else:
         dict[key].append((value))
 
-def plot_boxplot(energy_dict):
-    print "plotting violin_box_plot"
-    index = []
-    data = []
-    for i, (key, val) in enumerate(energy_dict.iteritems()):
-        index.append(key)
-        data.append(map(float, val))
-    fig, (ax, ax2) = plt.subplots(ncols=2)
-    ax.boxplot(data)
-    ax.set_xticklabels(index)
-    ax2.violinplot(data)
-    ax2.set_xticks(range(1, len(index) + 1))
-    ax2.set_xticklabels(index)
-    plt.savefig('box_violin_plot.pdf')
-
-def plot_histogramm(energy_dict):
-    print "plotting histogramm"
-    for key in energy_dict:
-        print key
-        x = []
-        for value in energy_dict[key]:
-            x.append(value)
-        sns_plot = sns.distplot(x)
-        sns_plot_all = sns.distplot(x)
-        sns_plot.figure.savefig("histogramm_" + key + ".png")
-        sns_plot_all.figure.savefig("histogramm_all.png")
-        sns_plot.figure.clf()
 
 # ------------------------------------------------- main script ------------------------------------------------------ #
 start_time = timeit.default_timer()
+
+# initialize the dict
 contact_count_dict = {'A': {}, 'C': {}, 'D': {}, 'E': {}, 'F': {}, 'G': {}, 'H': {}, 'I': {}, 'K': {}, 'L': {},
                       'M': {}, 'N': {}, 'P': {}, 'Q': {}, 'R': {}, 'S': {}, 'T': {}, 'U': {}, 'V': {}, 'W': {}, 'Y': {}}
 sub_contact_count_dict = {'A': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'K': 0, 'L': 0,
@@ -90,22 +59,21 @@ for dirpath, dir, files in os.walk(top=args.energy):
                 if line.startswith('ENGY'):
                     chain = line.split("\t")[1]
                     if bool(re.search(r'\d', chain)):
+                        print "ERROR: CHAIN is a Number, ", chain
                         continue
                     if prev_chain != "" and prev_chain != chain:
                         counter = 0
                         amino_dict_with_chains[prev_chain] = amino_seq
                         amino_seq = ""
-                        #print "#################################################################- SET COUNTER = 0"
-                        #print prev_chain
                     amino = line.split("\t")[3]
                     contacts = line.split("\t")[6].replace(" ", "").rstrip()
                     id = str(counter) + chain
                     id_list.append(id)
                     insert_into_data_structure(id, contacts, amino_contacts_dict)
                     amino_seq += amino
-                    # WIESO IST JEDES 2. ELEMENT LEEEER??
                     counter += 1
                     prev_chain = chain
+
             # one last time for adding the last chain aa_seq to the dict, or the first if only one chain
             amino_dict_with_chains[prev_chain] = amino_seq
 
@@ -115,7 +83,7 @@ for dirpath, dir, files in os.walk(top=args.energy):
                 chain = re.split(r'(\d+)', id)[-1]
                 contacts = amino_contacts_dict[id]
                 if (len(contacts) != len(amino_seq)):
-                    print "YOU FUCKED UP"
+                    print "ERROR: contact length is NOT equal to the sequence length!"
                     continue_counter += 1
                     continue
                 amino_seq = amino_dict_with_chains[chain]
@@ -124,15 +92,12 @@ for dirpath, dir, files in os.walk(top=args.energy):
                 for contact, amino_in_loop in zip(contacts, amino_seq):
                     # count all contacts except self contacts
                     if contact == "1" and contact_index != counter:
-                        nix = 0
-                        # print amino_now, amino_in_loop
                         contact_count_dict[amino_now][amino_in_loop] += 1
                     contact_index += 1
 
-print "print DICT: "
-# pprint(contact_count_dict)
 
 print timeit.default_timer() - start_time
+print "printing DICT: "
 
 for key, value in contact_count_dict.iteritems():
     print key
@@ -142,8 +107,6 @@ for key, value in contact_count_dict.iteritems():
     for key2, value2 in contact_count_dict[key].iteritems():
         sys.stdout.write(str(value2) + ",")
     print ""
-
-
 
 print "skipped ", continue_counter, " lines of EPs"
 
