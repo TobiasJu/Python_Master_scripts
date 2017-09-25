0acc4b9b716872892c3e9c05c0642cc6463dfe60
Twinstar <twinstar2@freenet.de>
2017-09-25 23:18:06 +0200

    added file for finding the membrane proteins

---
+++ Energy_Profile/get_all_membrane_pdb_files.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from shutil import copyfile
+import argparse
+import sys
+import os
+
+# argparse for information
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--pdbdir", help="input energy profile directory")
+parser.add_argument("-p", "--pdbtm", help="pdbtm_all.list.txt location")
+args = parser.parse_args()
+
+# sanity check
+if not len(sys.argv) > 1:
+    print "This script crawls thorug a given directory with subdirectorys and searches for all transmembran proteins"
+    parser.print_help()
+    sys.exit(0)
+
+# returns a list of all files in the dir with subdirs
+def list_files(dir):
+    file_list = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            file_list.append(os.path.join(root, name))
+    return file_list
+
+# ------------------------------------------------- main script ------------------------------------------------------ #
+
+pdbtm_list = []
+dir_file_list = list_files(args.pdbdir)
+
+with open(args.pdbtm, "r") as pdbtm_file_handle:
+    for line in pdbtm_file_handle:
+        name = line.split("_")[0]
+        pdbtm_list.append(name)
+
+for pdb_file in dir_file_list:
+    name1 = pdb_file.split("/")[0]
+    name2 = name1.split("pdb")[-1]
+    pdb_name = name2.split("ent.gz")
+    if pdb_name in pdbtm_list:
+        print "copying file: ", pdb_file
+        copyfile(pdb_file, "/ceph/sge-tmp/tjuhre/membranePDB/"+name1 )
