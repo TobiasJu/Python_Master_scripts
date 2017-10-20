4a0dae9f013c68fd4e901303dfd8d35a94c86794
Twinstar <twinstar2@freenet.de>
2017-10-20 23:33:22 +0200

    minor changes

--- shell/calculate_energy_bash.sh
+++ shell/calculate_energy_bash.sh
@@ -8,4 +8,7 @@ for f in *.pdb ; do java -jar ../../Python_Master_scripts/Java/SimpleECalc.jar "
 for f in *.ep2 ; do ../../Python_Master_scripts/Energy_Profile/add_aa_connections_for_one_file.py -f "$f"; done
 
 # plot energy profiles from SNPs vs original
-for f in *.cnn ; do ../../Python_Master_scripts/Energy_Profile/plot_energy_profile.py -e1 NP_000526.2_PMS2_2.ep2.cnn -e2 "$f"; done
+for f in *.cnn ; do ../Energy_Profile/plot_eps_and_snps.py -e1 NP_000242.1_MSH2.cnn -e2 "$f"; done
+
+
+../Python_Master_scripts/Energy_Profile/plot_eps_and_snps.py -e1 NP_000007.1_ACADM.ep2.cnn -e2
