20e1482c8990901c994fabfc769013fa61a331f8
Twinstar <twinstar2@freenet.de>
2017-10-20 23:32:32 +0200

    increades font size

--- Energy_Profile/plot_eps_and_snps.py
+++ Energy_Profile/plot_eps_and_snps.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-e1", "--energy_profile_1", help="input energy profile number 1")
 parser.add_argument("-e2", "--energy_profile_2", help="input energy profile number 2")
 parser.add_argument("-m", "--marker", help="SNP position number")
-parser.add_argument("-a", "--aminoacid", help="new Aminoacid SNP if any")
+parser.add_argument("-a", "--aminoacid", help="new Aminoacid SNP, if any")
 
 args = parser.parse_args()
 
@@ -41,9 +41,11 @@ def plotting(energy_file1, energy_file2, snp_pos, snp_aa):
         for line in inf:
             if "ENGY" in line:
                 spl = line.split('\t')
-                energy_list1.append(float(spl[5]))
-                pos_list.append(int(spl[2]))
-            line_count += 1
+                chain = spl[1]
+                if chain == "A":
+                    energy_list1.append(float(spl[5]))
+                    pos_list.append(int(spl[2]))
+                line_count += 1
     if pos_list[0] > snp_pos or pos_list[-1] < snp_pos:
         print "ERROR SNP is NOT in 3D Strukture"
         print "Strukture range: ", pos_list[0], pos_list[-1]
@@ -56,14 +58,16 @@ def plotting(energy_file1, energy_file2, snp_pos, snp_aa):
         for line in inf:
             if "ENGY" in line:
                 spl = line.split('\t')
-                energy_list2.append(float(spl[5]))
-                if int(spl[2]) == snp_pos:
-                    contacts = spl[-1].replace(" ", "")
-                    current_aa = spl[3]
-                    if Bio.SeqUtils.IUPACData.protein_letters_3to1[snp_aa] != current_aa:
-                        print "ERROR: WRONG SNP! in energy profile: ", energy_file2
-                        print "FOUND: ", current_aa, "expected: ", snp_aa
-                        sys.exit()
+                chain = spl[1]
+                if chain == "A":
+                    energy_list2.append(float(spl[5]))
+                    if int(spl[2]) == snp_pos:
+                        contacts = spl[-1].replace(" ", "")
+                        current_aa = spl[3]
+                        if Bio.SeqUtils.IUPACData.protein_letters_3to1[snp_aa] != current_aa:
+                            print "ERROR: WRONG SNP! in energy profile: ", energy_file2
+                            print "FOUND: ", current_aa, "expected: ", snp_aa
+                            sys.exit()
 
     highlight = int(snp_pos) - pos_list[0]
     labels = []
@@ -78,13 +82,23 @@ def plotting(energy_file1, energy_file2, snp_pos, snp_aa):
         energy_profile_list_2.append(energy2)
         energy_avg_1 += energy1
         energy_avg_2 += energy2
-        energy_diff.append(energy1 - energy2)
+        energy_diff.append(energy2 - energy1)
 
     y_p = np.array(energy_profile_list_2)
     y_h = np.array(energy_profile_list_1)
     y_d = np.array(energy_diff)
     x_h = np.array(labels)
     diff = str((energy_avg_1 / line_count) - (energy_avg_2 / line_count))[0:6]
+
+    SIZE = 14
+
+    plt.rc('font', size=SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=SIZE+2)  # fontsize of the axes title
+    plt.rc('axes', labelsize=SIZE+2)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=SIZE)  # fontsize of the figure title
 
     plt.rcParams["figure.figsize"] = (45, 10)
     plt.axis([pos_list[0], pos_list[-1], -40, 15])
@@ -93,6 +107,9 @@ def plotting(energy_file1, energy_file2, snp_pos, snp_aa):
     plt.plot(x_h, y_p, 'r-', label=energy_file_name2)
     plt.plot(x_h, y_h, 'b-', label=energy_file_name1)
     plt.plot(x_h, y_d, 'g-', label="diff " + diff)
+
+
+
     # plt.plot(x_h[12], y_p[0], 'g*')
     plt.axvspan(x_h[highlight - 1], x_h[highlight + 1], color='orange', alpha=0.5)
 
@@ -106,9 +123,9 @@ def plotting(energy_file1, energy_file2, snp_pos, snp_aa):
 
     print energy_avg_1/line_count, energy_avg_2/line_count, diff
     outfile = "comp_plot_" + str(energy_file_name1) + "_" + str(energy_file_name2)
-    plt.xlabel("ResNo")
-    plt.ylabel("Energy value")
-    plt.title("Energy compairson")
+    plt.xlabel("Position")
+    plt.ylabel("Energiewert")
+    plt.title("Energieprofil Vergleich")
     plt.legend()
     plt.savefig(outfile + ".pdf")
     plt.savefig(outfile + ".png")
